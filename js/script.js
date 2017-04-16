2a5f35d0ea1ed9360d410c0934c445935ce50c00
dsolis421 <snapshot8d@gmail.com>
2017-04-15 22:16:00 -0500

    v1 working shelter click

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 var $petfinderAPI = 'http://api.petfinder.com/';
 var $devkey = '3c73470956892905e562a55f0e113f50';
 
+function getShelter(id) {
+  $.getJSON($petfinderAPI + 'shelter.get?id=' + id + '&format=json&key=' + $devkey + '&callback=?')
+  .done(function(shelter){
+    console.log(shelter);
+  })
+  .error(function(err) {
+    console.log('Get shelter by ID error! ' + err);
+  });
+}
+
 function getSheltersZip(zip) {
-  $('#shelters').html('<h3>Finding families..</h3>');
+  $('#searchstatus').fadeOut("fast","linear", function() {
+    $('#searchstatus').html('<h3>Finding families...</h3>');
+    $('#searchstatus').fadeIn("slow","swing");
+  });
   $.getJSON($petfinderAPI + 'shelter.find?location=' + zip + '&format=json&key=' + $devkey + '&callback=?')
     .done(function(petApiData){
       console.log(petApiData);
-      $('#shelters').empty();
-      var shelters = petApiData.petfinder.shelters.shelter;
-      for (i in shelters) {
-        var listing = '<div class="shelter"><h4>' + shelters[i].name.$t + '</h4><div>See Family<div></div>';
-        $('#shelters').append(listing);
-      };
+      $('#searchstatus').fadeOut("slow","swing", function() {
+        $('searchstatus').empty();
+      });
+      $('#shelters').fadeOut("slow","swing", function() {
+        $('#shelters').empty();
+        var shelters = petApiData.petfinder.shelters.shelter;
+        for (i in shelters) {
+          var listing = '<div class="shelter" shelterid=' + shelters[i].id.$t + '><h4>' + shelters[i].name.$t + '</h4><div>See Family<div></div>';
+          $('#shelters').append(listing);
+        };
+        $('#shelters').fadeIn("slow","swing");
+        $('#shelters').on("click", ".shelter", function(){
+          getShelter($(this).attr('shelterid'));
+        });
+      });
     })
     .error(function(err){
-      console.log('Get shelters by zip error');
+      console.log('Get shelters by zip error! ' + err);
     });
 }
 
